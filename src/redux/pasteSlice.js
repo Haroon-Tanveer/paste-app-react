4b077b7100ed2724394455d55ca5c62169fcea04
Haroon-Tanveer <haroontmbw@gmail.com>
2025-08-31 14:56:03 +0500

    a

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -19,11 +19,33 @@ export const pasteSlice = createSlice({
         toast('Paste Created Successfully')
     },
     updateToPastes: (state,action) => {
+        const paste = action.payload;
+        const index = state.pastes.findIndex((item)=> item._id === paste._id);
+
+        if (index >= 0 ) {
+            state.pastes[index] = paste;
+            localStorage.setItem("pastes", JSON.stringify(state.pastes));
+
+            toast.success("Paste Updated");
+        }
     },
     resetAllPastes: (state, action) => {
+        state.pastes = []; 
+        localStorage.removeItem("pastes");
+
     },
     removeFromPastes: (state, action) => {
+        const pasteId = action.payload;
 
+        const index = state.pastes.findIndex((item) => item._id === pasteId);
+
+        if (index >= 0 ){
+            state.pastes.splice(index, 1);
+
+            localStorage.setItem("pastes", JSON.stringify(state.pastes));
+
+            toast.success("Paste Deleted");
+        }
     }
   },
 })
